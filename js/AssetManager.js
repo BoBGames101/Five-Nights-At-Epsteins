d36ff887b3ad98d6bc9d7923c1314a403516aa01
BoBGames101 <robertbasmajyan13@gmail.com>
2026-04-22 21:02:42 -0700

    Update AssetManager.js

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -1,1 +1,31 @@
+class AssetManager {
+    constructor() {
+        // This list tells the game where your pictures are
+        this.images = {
+            office: 'assets/images/office.png',
+            fan_icon: 'assets/images/fa3.png',
+            star: 'assets/images/star.png',
+            cutscene: 'assets/images/cutscene.png',
+            epstein: 'assets/images/epstein.png',
+            trump: 'assets/images/trump.png',
+            hawking: 'assets/images/hawking.png'
+        };
+
+        // This list tells the game where your sounds are
+        this.sounds = {
+            menu_music: 'assets/sounds/music3.ogg',
+            jumpscare: 'assets/sounds/jumpscare.mp3',
+            ambient: 'assets/sounds/ambience.mp3'
+        };
+
+        // This is for the camera static video
+        this.videos = {
+            static: 'assets/vedio/202512191935.webm'
+        };
+    }
+
+    getAsset(name) {
+        return this.images[name] || this.sounds[name] || this.videos[name];
+    }
+}
 
